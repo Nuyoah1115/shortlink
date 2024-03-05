7c27a285f1136cc1b2b333d562e70b41275436a3
Nuyoah <2278808968@qq.com>
2024-03-05 12:48:28 +0800

    fix: 修正日期格式

--- project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -50,13 +50,13 @@ public class ShortLinkPageRespDTO {
   /**
    * 有效期
    */
-  @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss", timezone = "GMT+8")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
   private Date validDate;
 
   /**
    * 创建时间
    */
-  @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss", timezone = "GMT+8")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
   private Date createTime;
 
   /**
