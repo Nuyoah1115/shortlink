43594eaf1a6a9e0054a74ba2ca52d53dd638c567
Nuyoah <2278808968@qq.com>
2024-03-07 08:43:11 +0800

    feature: 短链接跳转时过滤有效期失效的短链接并缓存空值

--- project/src/main/java/com/nageoffer/shortlink/project/common/constant/ShortLinkConstant.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/ShortLinkConstant.java
@@ -6,5 +6,8 @@ package com.nageoffer.shortlink.project.common.constant;
  @Description: 短链接常量类
  **/
 public class ShortLinkConstant {
+    /**
+     * 永久有效的短链接缓存有效期 默认一个月
+     */
     public static final long DEFAULT_CACHE_VALID_TIME = 2629800000L;
 }
--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -37,6 +37,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -219,7 +220,15 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkDO::getDelFlag, 0);
             ShortLinkDO shortLinkDO = baseMapper.selectOne(queryWrapper);
             if (shortLinkDO != null) {
-                stringRedisTemplate.opsForValue().set(String.format(GOTO_SHORT_LINK_KEY, fullShortUrl), shortLinkDO.getOriginUrl());
+                if (shortLinkDO.getValidDate()!=null&&shortLinkDO.getValidDate().before(new Date())){
+                    stringRedisTemplate.opsForValue()
+                            .set(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl), "-", 30, TimeUnit.SECONDS);
+                    return;
+                }
+                stringRedisTemplate.opsForValue().set(
+                        String.format(GOTO_SHORT_LINK_KEY, fullShortUrl),
+                        shortLinkDO.getOriginUrl(),
+                        LinkUtil.getLinkCacheValidDate(shortLinkDO.getValidDate()), TimeUnit.MILLISECONDS);
                 response.sendRedirect(shortLinkDO.getOriginUrl());
             }
         } finally {
