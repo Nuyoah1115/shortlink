14eae60f2298a582b7fc4d2229278623902b8394
Nuyoah <2278808968@qq.com>
2024-03-07 09:32:36 +0800

    docs: 短链接跳转原始链接方法注释

--- project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
@@ -29,6 +29,9 @@ import java.util.List;
 public class ShortLinkController {
     private final ShortLinkService shortLinkService;
 
+    /**
+     * 短链接跳转原始链接
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, HttpServletRequest request, HttpServletResponse response) throws IOException {
         shortLinkService.restoreUrl(shortUri,request,response);
