eeb43aa88df8cf108a2efbcc021d7c7c78e84c68
Nuyoah <2278808968@qq.com>
2024-03-18 19:03:22 +0800

    feature: 开发后管监控相关接口

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkStatsController.java
@@ -0,0 +1,56 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.ShortLinkActualRemoteService;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkGroupStatsAccessRecordReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkGroupStatsReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkStatsAccessRecordReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkStatsReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkStatsAccessRecordRespDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接监控控制层
+ */
+@RestController(value = "shortLinkStatsControllerByAdmin")
+@RequiredArgsConstructor
+public class ShortLinkStatsController {
+
+    private final ShortLinkActualRemoteService shortLinkActualRemoteService;
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats")
+    public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO statsReqDTO) {
+        return shortLinkActualRemoteService.oneShortLinkStats(statsReqDTO.getFullShortUrl(), statsReqDTO.getGid(), statsReqDTO.getStartDate(), statsReqDTO.getEndDate());
+    }
+
+    /**
+     * 访问分组短链接指定时间内监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/group")
+    public Result<ShortLinkStatsRespDTO> groupShortLinkStats(ShortLinkGroupStatsReqDTO groupStatsReqDTO) {
+        return shortLinkActualRemoteService.groupShortLinkStats(groupStatsReqDTO.getGid(), groupStatsReqDTO.getStartDate(), groupStatsReqDTO.getEndDate());
+    }
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record")
+    public Result<Page<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO accessRecordReqDTO) {
+        return shortLinkActualRemoteService.shortLinkStatsAccessRecord(accessRecordReqDTO.getFullShortUrl(), accessRecordReqDTO.getGid(), accessRecordReqDTO.getStartDate(), accessRecordReqDTO.getEndDate());
+    }
+
+    /**
+     * 访问分组短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record/group")
+    public Result<Page<ShortLinkStatsAccessRecordRespDTO>> groupShortLinkStatsAccessRecord(ShortLinkGroupStatsAccessRecordReqDTO groupStatsAccessRecordReqDTO) {
+        return shortLinkActualRemoteService.groupShortLinkStatsAccessRecord(groupStatsAccessRecordReqDTO.getGid(), groupStatsAccessRecordReqDTO.getStartDate(), groupStatsAccessRecordReqDTO.getEndDate());
+    }
+}
