88b9c0e766b28ae2652945b239ddee1ae6145788
Nuyoah <2278808968@qq.com>
2024-03-19 13:43:03 +0800

    feature: 开发网关服务&重构后管拦截

---
+++ gateway/src/main/java/com/nageoffer/shortlink/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Nuyoah
+ * @Date 2024/3/19
+ * @Description 网关启动类
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
